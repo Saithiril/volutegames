9ae41741aec03d6eb0b9d4d91fffbb48f0e74469
Saithiril <Saithiril@gmail.com>
2013-01-18 19:34:30 +0600

    1.0.0.3

--- news/admin.py
+++ news/admin.py
@@ -1,1 +1,5 @@
 # Create your views here.
+from django.contrib import admin
+from news.models import New
+
+admin.site.register(New)
--- news/models.py
+++ news/models.py
@@ -1,6 +1,13 @@
-from django.db import models
+﻿from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class News(models.Model):
-	pass
+class New(models.Model):
+	caption = models.CharField(max_length=30, verbose_name='Заголовок')
+	text_news = models.TextField(max_length=500, verbose_name='Текст')
+	pub_date = models.DateTimeField('date published', editable=False)
+	author = models.ForeignKey(User, editable=False)
+	class Meta:
+		verbose_name = "Новость"
+		verbose_name_plural = "Новости"
